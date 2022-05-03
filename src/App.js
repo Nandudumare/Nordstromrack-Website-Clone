3f142dabf2dc18ad4e133226f3eda1e8affbeff0
Nandu Dumare <dumarenandu70@gmail.com>
2022-05-03 12:47:31 +0530

    data-done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux';
+
+
+
+
+
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/Redux/Data/Reducer.js
@@ -0,0 +1,19 @@
+import data from "../../Data/data.json"
+
+const Data = data.data
+
+const initState = {
+    data: [...Data]
+}
+
+
+export const dataReducer = (state = initState, {
+    type,
+    payload
+}) => {
+    switch (type) {
+        default: {
+            return state
+        }
+    }
+}
