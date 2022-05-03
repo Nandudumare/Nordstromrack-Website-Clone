153f79025d8ad721843c59902cb7421a23037d3b
Monica1310 <100186167+Monica1310@users.noreply.github.com>
2022-05-03 23:07:33 +0530

    product page

--- src/App.js
+++ src/App.js
@@ -6,26 +6,15 @@ import Navbar from './Components/Navbar/Navbar';
 import React from 'react'
 import Slider from './Components/home/Slider/Slider'
 import Sliderdata from './Components/home/Slider/Slider' 
+import MainRoutes from './Routes/MainRoutes';
 
 const App = () => {
   return (
 
     <div className="App">
-      {/* <Navbar/> */}
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+
+      {/* <MainRoutes/>
+   */}
 
     <Slider></Slider> 
     </div>
--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Products from '../Components/InnerPages/ProductPages/Products'
 
 const MainRoutes = () => {
   return (
     <div>
+      <Products/>
       
     </div>
   )
