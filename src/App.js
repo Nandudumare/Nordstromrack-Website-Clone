8587a9e2a8ef0e09692ab6ffd1364a4c91406331
Nandu Dumare <dumarenandu70@gmail.com>
2022-05-05 13:51:35 +0530

    requird done - only cart remaining

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
 
    <>
 
-      {/* <MainRoutes/> */}
+      <MainRoutes/>
   
 
     {/* <Slider></Slider>  */}
--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -8,20 +8,32 @@ import Products from "../Components/InnerPages/ProductPages/Products";
 import Modals from "../Components/Navbar/Modals";
 import Side from "../Components/Navbar/Auth/Side";
 import Description from "../Components/InnerPages/Description/Description";
+import Purchase from "../Components/Navbar/Auth/Purchase";
+import WishList from "../Components/Navbar/Auth/WishList";
+import ShippingAddress from "../Components/Navbar/Auth/ShippingAddress";
+import PaymentMethods from "../Components/Navbar/Auth/PaymentMethod";
+import Password from "../Components/Navbar/Auth/Password";
+import Cart from "../Components/Navbar/Auth/Cart";
 
 const MainRoutes = () => {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
+      {/* <Purchase/> */}
+      {/* <WishList /> */}
       {/* <Products /> */}
       {/* <Modals /> */}
       {/* <Side /> */}
+      {/* <ShippingAddress/> */}
+      {/* <PaymentMethods /> */}
+      {/* <Password /> */}
+      {/* <Cart /> */}
 
       <Routes>
         <Route path="signin" element={<SignIn />} />
         <Route path="createacc" element={<CreateAcc />} />
-        <Route path="products" element={ <Products />}/>
-        <Route path="description" element={<Description/>}/>
+        <Route path="products" element={<Products />} />
+        <Route path="description" element={<Description />} />
       </Routes>
     </>
   );
