e57e670ebc9f16abff6b92b440f7a291c4e9e26b
Nandu Dumare <dumarenandu70@gmail.com>
2022-05-06 23:19:48 +0530

    wait

--- src/Components/InnerPages/Payment/Payment.jsx
+++ src/Components/InnerPages/Payment/Payment.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import { Spinner } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import styles from "./Pay.module.css";
 // import {ReactSpinner} from 'react-spinning-wheel';
 // import 'react-spinning-wheel/dist/style.css';
@@ -14,12 +15,11 @@ const Payment = () => {
   const [isHide, setIsHide] = useState(true);
 
   const formData = useSelector((state) => state.Data.formData);
-<<<<<<< Updated upstream
+
   console.log("formData:", formData);
-=======
+
   // console.log("formData:", formData);
   const navigate = useNavigate();
->>>>>>> Stashed changes
 
   // console.log(formData.fname,"namehere")
 
@@ -29,44 +29,29 @@ const Payment = () => {
     <div className={styles.div}>
       {!isHide ? (
         <div className={styles.paymentdiv}>
-<<<<<<< Updated upstream
-
-          <img style={{width:"80px"}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYfD7SshGzpdEJQU6y9ViAWpuHHXI97tTNEQ&usqp=CAU"/>
+          <img
+            style={{ width: "80px" }}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYfD7SshGzpdEJQU6y9ViAWpuHHXI97tTNEQ&usqp=CAU"
+          />
           <h1>Thank You!</h1>
-          <h4>You have Placed the  Order {"\n"} Successfully</h4>
-          <p>Your order has been proccessed and {"\n"}
-          will be diliver soon</p>
+          <h4>You have Placed the Order {"\n"} Successfully</h4>
+          <p>
+            Your order has been proccessed and {"\n"}
+            will be Delivery soon
+          </p>
 
-          
-
-    
-        
-          <p className={styles.add} >Name: {formData.fname}</p>
+          <p className={styles.add}>Name: {formData.fname}</p>
           {/* <p>{formData.fname}</p> */}
-      
 
           <p className={styles.add}>Delivery address: {formData.address}</p>
           <p></p>
 
-
-          <h4 >Happy Shopping {formData.fname} {formData.lname}</h4>
-          <button className={styles.button}>Home</button>
-=======
-          <h3>Thank you! {formData.fname}</h3>
-          <p>Your Payment is Successfully Done.</p>
-          <p>
-            The order Will deliver at {formData.address} by tommarro at 8'O
-            clock .
-          </p>
-          <button
-            className={styles.button}
-            onClick={() => {
-              navigate("/");
-            }}
-          >
+          <h4>
+            Happy Shopping {formData.fname} {formData.lname}
+          </h4>
+          <button className={styles.button} onClick={() => navigate("/")}>
             Home
           </button>
->>>>>>> Stashed changes
         </div>
       ) : (
         <div>
